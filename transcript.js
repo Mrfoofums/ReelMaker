46b47c2dbde7ecce17ebce3c2c6590df670f3b9e
Forrest <forrestknightjr@gmail.com>
2023-09-14 20:30:36 -0400

    transcript.js finally does something intelligent

--- transcript.js
+++ transcript.js
@@ -5,17 +5,17 @@ const fs = require('fs');
 async function findInterestingSectionsInTranscriptFromFile(filename) {
     try {
       const transcript = fs.readFileSync(filename, 'utf8');
-      const sections = splitTranscriptIntoSections(transcript, 2 * 60); // Split into sections based on input time interval
+      const sections = splitTranscriptIntoSections(transcript, 7 * 60); // Split into sections based on input time interval
       const responses = [];
   
       for (let i = 0; i < sections.length; i++) {
         console.log(`Executing iteration ${i}`);
         const section = sections[i];
-        console.log(`Timestamp ${section.timestamp} & Section:  ${section.text}`)
-        // const response = await analyzeTranscriptSection(section);
-        const response = {
-          data: choices = [1,2,3]
-        };
+        console.log(`Section ${i}:  ${section.text}`)
+        const response = await analyzeTranscriptSection(section);
+        // const response = {
+        //   data: choices = [1,2,3]
+        // };
   
         if (response && response.data.choices && response.data.choices.length > 0) {
           const timestamp = section.timestamp;
@@ -34,7 +34,8 @@ async function findInterestingSectionsInTranscriptFromFile(filename) {
   function splitTranscriptIntoSections(transcript, sectionDurationInSeconds) {
     const lines = transcript.split('\n');
     const sections = [];
-    let currentSection = { timestamp: '', text: '' };
+    let currentSection = {text: '' };
+    let currentTimestamp = '';
     let prevTimestampInSeconds = 0;
   
     for (let i = 0; i < lines.length; i++) {
@@ -43,7 +44,7 @@ async function findInterestingSectionsInTranscriptFromFile(filename) {
       //If the current line is a timestamp, process the timestamp
       if (line.match(/^\d{2}:\d{2}:\d{2}:\d{2}\s+-/)) {
         console.log(`The line that matched the regex ${line} `)
-        currentSection.timestamp = line;
+        currentTimestamp = line;
 
         // Extract the timestamp in seconds
         const timestampParts = line.split(' ')[0].split(':').map(Number);
@@ -57,10 +58,15 @@ async function findInterestingSectionsInTranscriptFromFile(filename) {
         // Check if the current timestamp is more than 7 minutes ahead of the previous one
         if (timestampInSeconds - prevTimestampInSeconds > sectionDurationInSeconds) {
           sections.push(currentSection);
-          currentSection = { timestamp: '', text: '' };
+          currentSection = { text: '' };
+          //begining of next section
+          currentSection.text += line + '\n';
           prevTimestampInSeconds = timestampInSeconds;
+        } 
+        else{
+          //it's a timestamp but inside our timing
+          currentSection.text+=line + '\n';
         }
-  
         // currentSection.timestamp = line;
         // prevTimestampInSeconds = timestampInSeconds;
       } else {
@@ -75,14 +81,15 @@ async function findInterestingSectionsInTranscriptFromFile(filename) {
   
     return sections;
   }
-  
+  const prompt = 'Find which parts of this transcript are interesting. Give your response in this format. 1) The timestamp 2) Why it is  interesting 3) The Text from the transcript :\n\n';
+
   async function analyzeTranscriptSection(section) {
     try {
       const apiUrl = 'https://api.openai.com/v1/engines/text-davinci-003/completions';
       const response = await axios.post(
         apiUrl,
         {
-          prompt: `Find the interesting parts of this transcript:\n\n${section.text}`,
+          prompt: `${prompt}${section.text}`,
           max_tokens: 150, // Adjust based on your token limit
         },
         {
